4615904ac65c2f38e900b1b16d0a3690b2d0ae44
Salad Climbing <84188441+SaladClimbing@users.noreply.github.com>
2022-07-10 21:27:23 -0400

    Initial commit

---
+++ Window.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Forms;
+
+namespace SlipstreamEngine
+{
+    public class Window : Form
+    {
+        Form form;
+        RichTextBox console;
+        
+        public Window()
+        {
+            form = new Form();
+            console = new RichTextBox();
+
+            this.Controls.Add(console);
+            this.Resize += Window_Resize;
+            Window_Load(console);
+        }
+
+        private void Window_Load(RichTextBox console)
+        {
+            console.Size = this.ClientSize;
+            console.Top = 0;
+            console.Left = 0;
+            console.BackColor = Color.Black;
+            console.ForeColor = Color.White;
+            console.WordWrap = false;
+            console.Font = new Font("Consolas", 12);
+        }
+
+        private void Window_Resize(object sender, EventArgs e)
+        {
+            console.Size = this.ClientSize;
+        }
+
+    }
+}
