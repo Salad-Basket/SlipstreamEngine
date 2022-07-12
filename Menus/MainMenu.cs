562b8ca45f7f8c66fc16b3a14c87fff21bb57097
Salad Climbing <84188441+SaladClimbing@users.noreply.github.com>
2022-07-11 23:07:36 -0400

    Main menu changes
    Allow for an action to be invoked when player plays

--- Menus/MainMenu.cs
+++ Menus/MainMenu.cs
@@ -9,14 +9,15 @@ public class MainMenu
     public bool centered { get; private set; }
     private SlipstreamEngine.InputManager.InputManager im = SlipstreamEngine.InputManager.InputManager.GetInstance;
 
-    public MainMenu(string gameName, bool optionsMenu = false, bool centered = true)
+    public MainMenu(string gameName, bool centered = true, bool optionsMenu = false)
     {
         this.optionsMenu = optionsMenu;
         this.gameName = gameName;
+        this.centered = centered;
     }
 
     // Displays the menu to the window and waits for a response
-    public void displayMenu(Window window, Action action)
+    public void displayMenu(Window window, Action playAction)
     {
         string fullMenu = string.Empty;
         string options;
@@ -25,15 +26,16 @@ public class MainMenu
         fullMenu += this.gameName;
         fullMenu += "\n" + options;
 
+        this.im.addAction(menuInput(window, playAction));
         window.Write(fullMenu);
-        this.im.addAction(menuInput(window));
+        if (this.centered) window.center();
     }
 
-    private Action menuInput(Window window) => () =>
+    private Action menuInput(Window window, Action playAction) => () =>
     {
         int keyCode = this.im.checkKey(this.im.key);
 
-        if (keyCode == 1) window.Write("Played");
+        if (keyCode == 1) playAction.Invoke();
         if (keyCode == 2 && optionsMenu) window.Write("OPTIONS!");
         else if (keyCode == 2 && !optionsMenu) Application.Exit();
         if (keyCode == -2) Application.Exit();
