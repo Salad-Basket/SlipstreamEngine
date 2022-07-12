16ebd417394cde58bdd3913226da08cae2037b60
Salad Climbing <84188441+SaladClimbing@users.noreply.github.com>
2022-07-12 19:48:09 -0400

    Fixed player variables

--- PlayerContainer/Player.cs
+++ PlayerContainer/Player.cs
@@ -1,8 +1,8 @@
 ﻿namespace SlipstreamEngine.PlayerContainer;
 public  class Player
 {
-    public float health { get { return health; } set { health = value; } }
-    public float speed { get { return speed; } set { speed = value; } }
+    public float health { get; private set; }
+    public float speed { get; private set; }
 
     public Player(float health = 0f, float speed = 0f)
     {
